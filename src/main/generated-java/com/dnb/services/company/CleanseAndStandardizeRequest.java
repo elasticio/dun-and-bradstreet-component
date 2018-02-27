f909ea28ede7cc32bf9efc73a4ef8cb7eda9e089
A3a3e1 <pavel.voropaiev@elastic.io>
2018-02-27 18:55:46 +0200

    fixed XmlRootElement

--- src/main/generated-java/com/dnb/services/company/CleanseAndStandardizeRequest.java
+++ src/main/generated-java/com/dnb/services/company/CleanseAndStandardizeRequest.java
@@ -1,10 +1,7 @@
 
 package com.dnb.services.company;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -34,6 +31,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "CleanseAndStandardizeRequest", namespace = "http://services.dnb.com/CompanyServiceV2.0", propOrder = {
     "cleanseAndStandardizeRequestDetail"
 })
+@XmlRootElement(name = "CleanseAndStandardizeRequest", namespace = "http://services.dnb.com/CompanyServiceV2.0")
 public class CleanseAndStandardizeRequest
     extends Request
 {
