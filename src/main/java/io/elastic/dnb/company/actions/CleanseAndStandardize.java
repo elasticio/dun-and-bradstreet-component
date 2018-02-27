5edf0d2c372b24532595bfefefd33daad249de3e
A3a3e1 <pavel.voropaiev@elastic.io>
2018-02-27 18:40:31 +0200

    Implemented CleanseAndStandardize

---
+++ src/main/java/io/elastic/dnb/company/actions/CleanseAndStandardize.java
@@ -0,0 +1,84 @@
+package io.elastic.dnb.company.actions;
+
+import com.dnb.services.company.CleanseAndStandardizeRequest;
+import com.dnb.services.company.CleanseAndStandardizeResponse;
+import com.dnb.services.company.GetCleanseMatchRequest;
+import com.dnb.services.company.GetCleanseMatchResponse;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.JSON;
+import io.elastic.api.Message;
+import io.elastic.api.Module;
+import io.elastic.dnb.GenericSOAPClient;
+import io.elastic.dnb.Utils;
+import io.elastic.dnb.soap.client.EndpointUrl;
+import io.elastic.dnb.soap.client.SoapAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class CleanseAndStandardize implements Module {
+
+    protected static final Logger logger = LoggerFactory.getLogger(CleanseAndStandardize.class);
+
+    @Override
+    public void execute(ExecutionParameters parameters) {
+
+        JsonObject configuration = parameters.getConfiguration();
+        Message data = null;
+        JsonObject jsonDataObject = null;
+
+        JsonObject body = parameters.getMessage().getBody();
+        logger.info("About to call DnB API. Request message: {}", body.toString());
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        try {
+            CleanseAndStandardizeRequest cleanseAndStandardizeRequest = mapper.readValue(body.toString(), CleanseAndStandardizeRequest.class);
+
+            SOAPMessage response = new GenericSOAPClient.Builder()
+                    .setRequestClass(CleanseAndStandardizeRequest.class)
+                    .setBodyObject(cleanseAndStandardizeRequest)
+                    .setEndpointUrl(EndpointUrl.COMPANY_5_0)
+                    .setSoapAction(SoapAction.CLEANSE_AND_STANDARDIZE_MATCH)
+                    .setUsername(Utils.getUsername(configuration))
+                    .setPassword(Utils.getPassword(configuration))
+                    .call();
+
+            JAXBElement jaxbElement = new GenericSOAPClient.Builder().bindToJaxb(CleanseAndStandardizeResponse.class, response);
+            CleanseAndStandardizeResponse cleanseAndStandardizeResponse = (CleanseAndStandardizeResponse) jaxbElement.getValue();
+
+            ObjectMapper responseMapper = new ObjectMapper();
+            StringWriter sw = new StringWriter();
+            responseMapper.writeValue(sw, cleanseAndStandardizeResponse);
+            jsonDataObject = JSON.parseObject(sw.toString());
+
+            data = new Message.Builder().body(jsonDataObject).build();
+
+        } catch (JAXBException e) {
+            throw new ClassCastException("Can't map JSON object to MatchRequest XML");
+        } catch (IOException | XMLStreamException | SOAPException e) {
+            e.printStackTrace();
+            data = (new Message.Builder())
+                    .body(Json.createObjectBuilder()
+                            .add("result", e.getMessage())
+                            .build())
+                    .build();
+            parameters.getEventEmitter().emitException(e);
+        }
+
+        parameters.getEventEmitter().emitData(data);
+
+    }
+
+}
--- src/main/java/io/elastic/dnb/soap/client/SoapAction.java
+++ src/main/java/io/elastic/dnb/soap/client/SoapAction.java
@@ -4,6 +4,7 @@ public enum SoapAction {
 
     MATCH("http://services.dnb.com/CompanyService/V5.0/Match"),
     GET_CLEANSE_MATCH("http://services.dnb.com/CompanyService/V5.0/GetCleanseMatch"),
+    CLEANSE_AND_STANDARDIZE_MATCH("http://services.dnb.com/CompanyService/V5.0/CleanseAndStandardize"),
     NEWS_AND_MEDIA_PRODUCT("http://services.dnb.com/NewsAndMediaProductService/V3.0/OrderProduct"),
     FIND_COMPETITOR("http://services.dnb.com/EntityListService/V6.4/FindCompetitor");
 
