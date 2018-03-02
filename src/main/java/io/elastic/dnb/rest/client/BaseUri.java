43c9358f011e27d4ec5f985a899343fe28ab745e
A3a3e1 <pavel.voropaiev@elastic.io>
2018-03-02 13:00:24 +0200

    added SocialMedia

--- src/main/java/io/elastic/dnb/rest/client/BaseUri.java
+++ src/main/java/io/elastic/dnb/rest/client/BaseUri.java
@@ -3,8 +3,8 @@ package io.elastic.dnb.rest.client;
 public enum BaseUri {
 
     BASE_AUTH_URI("https://direct.dnb.com/Authentication/V2.0/"),
-    SOCIAL_MEDIA_PROFILES("https://direct.dnb.com/dataexchange/"),
-    PRODUCTS("https://direct.dnb.com/V2.2/organizations/804735132/products?CountryISOAlpha2Code=US");
+    SOCIAL_MEDIA_PROFILES("https://direct.dnb.com/dataexchange/");
+//    PRODUCTS("https://direct.dnb.com/V2.2/organizations/804735132/products?CountryISOAlpha2Code=US");
 
     private String baseUriValue;
 
